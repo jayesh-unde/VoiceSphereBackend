4d9d907d2d1ebafc2733015b8faf68dfd101339d
jayesh-unde <113255761+jayesh-unde@users.noreply.github.com>
2024-07-04 04:08:46 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -6,16 +6,16 @@ const roomsController = require('./controllers/rooms-controller');
 
 router.post('/api/send-otp',authController.sendOtp);
 router.post('/api/verify-otp',authController.verifyOtp);
-router.post('/api/activate',authMiddlewares,activateController.activate);
+router.post('/api/activate',activateController.activate);
 router.get('/api/refresh',authController.refresh);
-router.post('/api/logout',authMiddlewares,authController.logout);
-router.post('/api/rooms',authMiddlewares,roomsController.create);
-router.get('/api/rooms',authMiddlewares,roomsController.index);
+router.post('/api/logout',authController.logout);
+router.post('/api/rooms',roomsController.create);
+router.get('/api/rooms',roomsController.index);
 
-router.get('/api/rooms/:roomId', authMiddlewares, roomsController.show);
+router.get('/api/rooms/:roomId', roomsController.show);
 router.post('/api/find-user',authController.findUser);
 router.post('/api/profile',authController.findUserData);
 router.post('/api/send-otp-email',authController.sendOtpEmail);
 router.post('/api/login-email',authController.loginEmail);
-router.post('/api/leave-room',authMiddlewares,roomsController.leaveRoom);
+router.post('/api/leave-room',roomsController.leaveRoom);
 module.exports = router;
